620d395b8734282d122fd9c1ff12313227419c4c
yangirxd <yangirxd@gmail.com>
2023-12-29 20:43:19 +0300

    задача с литкода

---
+++ Test/Test/Programm.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Diagnostics.Metrics;
+
+namespace Test
+{
+    public class Programm
+    {
+        public static void Main() {
+            
+        }
+    }
+
+    class Player 
+    { 
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+
+    class Team
+    {
+        Player[] players = new Player[11];
+
+        public Player this[int index] {
+            get 
+            {
+                return players[index];
+            }
+            set 
+            {
+                players[index] = value;
+            }
+        }
+
+    }
+    }
